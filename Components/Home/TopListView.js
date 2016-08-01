aba0075655d26fa3e0788e9014d7354b3fdf49f3
zhuxf <18601345744@163.com>
2016-08-01 16:55:39 +0800

    test

---
+++ Components/Home/TopListView.js
@@ -0,0 +1,49 @@
+/* jshint esversion: 6 */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ListView
+} from 'react-native';
+
+class Shop extends Component {
+
+  constructor(props){
+    super(props);
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row1' , 'row2']),
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+});
+
+module.exports = Shop;
